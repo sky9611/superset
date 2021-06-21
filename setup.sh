625f9a6952754014709260d443e3f3178a1696a3
Tianhao <tianhaowu@microsoft.com>
2021-06-21 12:47:46 +0800

    upadate setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Launch superset
 gunicorn "superset.app:create_app()"
+
+# Create an admin user
+superset fab create-admin --username admin --firstname Superset --lastname Admin --email admin@superset.com --password admin
+
+# Initialize the database
 superset db upgrade
+
+# Create default roles and permissions
 superset init
